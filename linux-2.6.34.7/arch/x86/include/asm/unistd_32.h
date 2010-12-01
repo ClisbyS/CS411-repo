8b4c8aac385b42da926b09f61878011f0396396a
Matt Thomas <baddack@gmail.com>
2010-11-30 16:06:14 -0800

    fixed some changes from previous project that broke this project

--- linux-2.6.34.7/arch/x86/include/asm/unistd_32.h
+++ linux-2.6.34.7/arch/x86/include/asm/unistd_32.h
@@ -343,12 +343,10 @@
 #define __NR_rt_tgsigqueueinfo	335
 #define __NR_perf_event_open	336
 #define __NR_recvmmsg		337
-#define __NR_sys_get_slob_amt_claimed 338
-#define __NR_sys_get_slob_amt_free 339
 
 #ifdef __KERNEL__
 
-#define NR_syscalls 340
+#define NR_syscalls 338
 
 #define __ARCH_WANT_IPC_PARSE_VERSION
 #define __ARCH_WANT_OLD_READDIR
